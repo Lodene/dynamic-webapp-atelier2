f92241ef113fbca1e33641ebcc43f7b43238de8f
David Pichard <davidpichard@mb.local>
2024-05-23 13:52:32 +0200

    feat: new endpoint for return all cards

--- back/src/main/java/com/sp/controller/CardController.java
+++ back/src/main/java/com/sp/controller/CardController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.http.HttpStatus;
 
 import com.sp.service.CardService;
@@ -81,4 +82,10 @@ public class CardController {
         }
         return ResponseEntity.ok("Card sold successfully.");
     }
+
+
+    @GetMapping("all")
+	public Iterable<Card> getCards() {
+		return cardService.getAll();
+	}
 }
